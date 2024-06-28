bf63f010dfcb5373c4df1af8137b8985371bb70b
Alonso <amuc23@gmail.com>
2024-06-28 19:00:29 -0400

    base de videojuegos y consolas

--- megagames/models.py
+++ megagames/models.py
@@ -1,3 +1,25 @@
+
 from django.db import models
 
-# Create your models here.
+class Videojuego(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField("Nombre", max_length=100)
+    descripcion = models.TextField("Descripción")
+    precio = models.IntegerField("Precio")
+    consolas = models.CharField("Consolas", max_length=100)
+    stock = models.IntegerField("Stock")
+    url_imagen = models.URLField("URL de la Imagen", max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Consola(models.Model):
+    idconsola = models.AutoField(primary_key=True)
+    nombre = models.CharField("Nombre", max_length=100)
+    url_imagen = models.URLField("URL de la Imagen", max_length=200)
+    stock = models.IntegerField("Stock")
+    precio = models.DecimalField("Precio", max_digits=8, decimal_places=2)
+    
+    def __str__(self):
+        return self.nombre
--- megagames/views.py
+++ megagames/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
+from .models import Videojuego,Consola
+
 # Create your views here.
 def index (request):
     context ={}
     return render (request, 'megagames/index.html',context)
 
 
-def juego (request):
-    context ={}
-    return render (request, 'megagames/juego.html',context)
+def juego(request):
+    query = request.GET.get('q', '')
+    if query:
+        videojuegos = Videojuego.objects.filter(nombre__icontains=query)
+    else:
+        videojuegos = Videojuego.objects.all()
+    
+    context = {"videojuegos": videojuegos}
+    return render(request, 'megagames/juego.html', context)
 
-def consolas (request):
-    context ={}
-    return render (request, 'megagames/consolas.html',context)
+def consolas(request):
+    consolas = Consola.objects.all()  # Fetch all instances of Consola
+    context = {"consolas": consolas}  # Pass instances to the context dictionary
+    return render(request, 'megagames/consolas.html', context)
 
 def jugetes (request):
     context ={}
