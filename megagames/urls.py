ec7caeeeaf06e5eaa2036fa8eff8da2c1a1a1621
Alonso <amuc23@gmail.com>
2024-06-29 22:53:37 -0400

    cruds completos

--- megagames/urls.py
+++ megagames/urls.py
@@ -13,9 +13,27 @@ urlpatterns = [
     path('juego/<str:nombre>/', views.detalle_juego, name='detalle_juego'),
     path('consola/<str:nombre>/', views.detalle_consola, name='detalle_consola'),
     path('juegoraw' , views.juegoraw , name='juegoraw' ),
+
+    ##### CRUD JUEGOS
     path('lista_videojuegos/', views.lista_videojuegos, name='lista_videojuegos'),
     path('videojuego_crear/', views.videojuego_crear, name='videojuego_crear'),
     path('editar_videojuego/<str:nombre>/', views.editar_videojuego, name='editar_videojuego'),
     path('eliminar_videojuego/<str:nombre>/', views.eliminar_videojuego, name='eliminar_videojuego'),
+
+    ###### CRUD CONSOLAS
+    path('lista_consolas/', views.lista_consolas, name='lista_consolas'),
+    path('consola_crear/', views.consola_crear, name='consola_crear'),
+    path('editar_consola/<str:nombre>/', views.editar_consola, name='editar_consola'),
+    path('eliminar_consola/<str:nombre>/', views.eliminar_consola, name='eliminar_consola'),
+
+    ##### CRUD JUGUETES
+
+    path('lista_juguetes/', views.lista_juguetes, name='lista_juguetes'),
+    path('juguete_crear/', views.juguete_crear, name='juguete_crear'),
+    path('editar_juguete/<str:nombre>/', views.editar_juguete, name='editar_juguete'),
+    path('eliminar_juguete/<str:nombre>/', views.eliminar_juguete, name='eliminar_juguete'),
 ]
+
+    
+
     
